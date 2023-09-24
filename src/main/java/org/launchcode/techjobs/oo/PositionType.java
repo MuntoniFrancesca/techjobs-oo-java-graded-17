683786a42c1e39a1673b1d6af1f08d03e47e6cbd
MuntoniFrancesca <francesca.muntoni@live.com>
2023-09-24 13:24:28 -0500

    Finished task 6 1 test failed

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -7,19 +7,6 @@ public class PositionType extends JobField {
     public PositionType(String value) {
         super(value);
     }
-//    private int id;
-//    private static int nextId = 1;
-//    private String value;
-//
-//    public PositionType() {
-//        id = nextId;
-//        nextId++;
-//    }
-//
-//    public PositionType(String value) {
-//        this();
-//        this.value = value;
-//    }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 //    @Override
@@ -35,24 +22,5 @@ public class PositionType extends JobField {
         PositionType that = (PositionType) o;
         return getId() == that.getId() && Objects.equals(getValue(), that.getValue());
     }
-
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id, value);
-//    }
-
-    // Getters and Setters:
-
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public String getValue() {
-//        return value;
-//    }
-//
-//    public void setValue(String value) {
-//        this.value = value;
-//    }
 
 }
